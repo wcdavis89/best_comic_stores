5f59b4c41fc815e2924da0cb95671250a404790c
Whittney Davis <wcdavis89@yahoo.com>
2019-12-11 15:55:16 +0000

    adding info to methods

--- lib/best_comic_stores/controller.rb
+++ lib/best_comic_stores/controller.rb
@@ -12,29 +12,37 @@ class BestComicStores::CLI
       @stores = BestComicStores::Stores.all
   #it might work, I've moved the method to stores.rb
   #also, I need a spacer of some sort
+  @stores.each.with_index(1) {|store, i| puts "#{i}. #{store.name}"}
   end
   
     def menu
       
       input = nil
       while input != "exit"
-      puts "Which one did you want to hear about? Enter the number, type 'list' to see the list again, or type exit to leave the application: "
+      puts "Which one did you want to hear about? Enter the number, type 'list' to see the list again, or type 'exit' to leave the application: "
         input=gets.strip.downcase
-        case input
-        when "1"
-          puts "More info on number 1"
-        when "2"
-          puts "More info on number 2"
-        when "list"
-          list_stores
-        else
-          #this broke "exit", nvm removed .menu
-          puts "Oops! Our list doesn't reach that far, here are the choices again."
-          list_stores
+        if input.to_i > 0
+            puts @stores[input.to_i-1].name
+          elsif input == "list"
+             list_stores
+          else 
+            puts "Oops! Our list doesn't reach that far, here are the choices again."
+        
         end
       end
     end
-  
+    
+    # def menu_2
+      
+    #   input = nil
+    #   while input != "exit"
+    #   puts "What do you want to know about #{@stores[input.to_i-1].name}? Type 'location', 'website', or 'description'. Type 'back' to go back to the main menu or 'exit' to leave the application."
+    #   if input.to_i > 0 
+    #       puts @stores[input.to_i-1].location
+    #       puts @stores[input.to_i-1].website
+    #       puts @stores[input.to_i-1].description
+           
+           
   def goodbye
     puts "Safe travels!"
   end
--- lib/best_comic_stores/stores.rb
+++ lib/best_comic_stores/stores.rb
@@ -1,22 +1,20 @@
 class BestComicStores::Stores
-  
   attr_accessor :name, :location, :website, :description
   
   def self.all
     #stores object?
-    puts <<-DOC.gsub /^\s*/,''
-
-      1. Atomic Books 
-      2. Forbidden Planet
-      DOC
+    # puts <<-DOC.gsub /^\s*/,''
+    #   1. Atomic Books 
+    #   2. Forbidden Planet
+    #   DOC
   
-  store_1=Stores.new
+  store_1=self.new
   store_1.name = "Atomic Books"
   store_1.location = "Baltimore, MD"
   store_1.website = "https://atomicbooks.com/"
   store_1.description = "Surely the platonic ideal of an indie comics store, Atomic Books is pretty much the be-all and end-all for comic fans of the non-superhero variety. While the shop hardly confines itself to comics, it is the likeliest place to find lots of underground stuff, from small-press single issues to DIY mini comics and zines. Its mail order business is legend, and we pore over the place’s weekly newsletter. Purists may turn up their noses at the wide range of other products Atomic sells: literary novels, art books, design toys, crafting guides, music, DVDs… etc. But what matters is that its entire stock is meticulously curated by its whip-smart staff, which means the junk quotient is minimal. Their wonderful events feature readings, a weekly knitting night, and a book club. Their blog is actually worth reading. And just in case you weren’t impressed yet, Atomic is also the only store in the world that has been given the esteemed privilege of collecting John Waters’s mail."
   
-  store_2=Stores.new
+  store_2=self.new
   store_2.name = "Forbidden Planet"
   store_2.location = "New York, NY"
   store_2.website = "https://www.fpnyc.com/"
@@ -25,4 +23,7 @@ class BestComicStores::Stores
   [store_1, store_2]
   end
   
+  # def store_info
+  #   @store_info = BestComicStores::StoreInfo.all
+  # end
 end
