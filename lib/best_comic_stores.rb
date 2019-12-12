534b515f51d7c4b07377401cadded27ccd6a35df
Whittney Davis <wcdavis89@yahoo.com>
2019-12-12 17:24:52 +0000

    minor edits

--- lib/best_comic_stores.rb
+++ lib/best_comic_stores.rb
@@ -1,3 +1,7 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
 require_relative "./best_comic_stores/version"
 require_relative './best_comic_stores/controller'
 require_relative './best_comic_stores/stores'
