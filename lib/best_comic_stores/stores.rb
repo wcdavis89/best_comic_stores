c165e44a6993287b8db0426977de2cdab2961f6d
Whittney Davis <wcdavis89@yahoo.com>
2019-12-13 19:05:29 +0000

    editing names

--- lib/best_comic_stores/stores.rb
+++ lib/best_comic_stores/stores.rb
@@ -1,28 +1,31 @@
 class BestComicStores::Stores
-  attr_accessor :name_loc, :website, :description
+  attr_accessor :name, :location, :website, :description
   
   def self.all
-    self.scrape_flavorwire
+    self.scrape_fordors
   end
   
-  def self.scrape_flavorwire
-    stores = []
-    locations = []
-    websites = []
-    description = []
-    
-    doc = Nokogiri::HTML(open("https://www.fodors.com/news/photos/11-super-places-that-comic-book-fans-must-visit"))
-    
-    name = doc.search("h2").each {|store| stores << store.text}
-    location = doc.search("h3 span").each {|location| locations << location.text.strip}
-    website = doc.search("p a").each{|store|websites << store.attr("href")}
-    description = doc.search("p").text
-   binding.pry
-  
-    stores << self.scrape_flavorwire
-    stores.pop
-    websites.shift
-    stores
-  end
+      def self.scrape_fordors
+        names = []
+        locations = []
+        websites = []
+        descriptions = []
+        
+        doc = Nokogiri::HTML(open("https://www.fodors.com/news/photos/11-super-places-that-comic-book-fans-must-visit"))
+        
+        name = doc.search("div.subtitle h2").each {|store| names << store.text.strip}
+        location = doc.search("h3 span").each {|location| locations << location.text.strip}
+        website = doc.search("p a").each{|store|websites << store.attr("href")}
+        description = doc.search("div.slide-text p").each {|desc| descriptions << desc.text}
+        
+        websites.shift
+        names.pop
+        names
+        locations
+        websites  
+        descriptions
+        binding.pry
+        
+      end
   
 end
